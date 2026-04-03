f594f017e04cb67671806782646ce8efc0802750
Gossaye Bireda <gosa.dev99@gmail.com>
2026-04-02 21:47:45 -0700

    feat: Level 16 - Lead Research Pipeline complete

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import LeadResearch from './leads/LeadResearch';
+
+function Router() {
+  const path = window.location.pathname;
+  if (path.startsWith('/leads')) return <LeadResearch />;
+  return <App />;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
